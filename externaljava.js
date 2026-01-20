44bdc773362abe2144724b4acd9f34731d649fa0
MiishkaEugenioMNE <eugenio.mn@obmontessori.edu.ph>
2026-01-20 13:27:41 +0800

    Add files via upload

---
+++ externaljava.js
@@ -0,0 +1,24 @@
+function add(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = Number(num1) + Number(num2);
+    window.alert(sum); 
+}
+function subtract(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var difference = Number(num1) - Number(num2);
+    window.alert(difference); 
+}
+function product(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var product = Number(num1) * Number(num2);
+    window.alert(product); 
+}
+function quotient(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var quotient = Number(num1) / Number(num2);
+    window.alert(quotient); 
+}
